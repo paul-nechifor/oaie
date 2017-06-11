722ee0dfe31b0483e0548cc323ce9521e8828754
Paul Nechifor <paul@nechifor.net>
2017-06-11 11:49:27 -0400

    Add random player.

--- src/GomokuBot.js
+++ src/GomokuBot.js
@@ -1,12 +1,26 @@
+const _ = require('underscore');
 const AbstractBot = require('./AbstractBot');
 
+const MOVE_EMPTY = -1;
+const MOVE_BLACK = 0;
+const MOVE_WHITE = 1;
+const SIZE = 15;
+const FULL_SIZE = SIZE * SIZE;
+
 module.exports = class GomokuBot extends AbstractBot {
+  constructor(...args) {
+    super(...args);
+    this.board = _.range(FULL_SIZE).map(() => MOVE_EMPTY);
+  }
+
   getGameType() {
     return 'Gomoku';
   }
 
   handleMsgStarted(msg) {
-    console.log('startedddddd');
+    if (this.index === 0) {
+      this.sendMove();
+    }
   }
 
   handleMsgNumberOfPlayers(msg) {
@@ -16,5 +30,32 @@ module.exports = class GomokuBot extends AbstractBot {
   handleMsgYouAre(msg) {
     console.log('You are index:%d id:%s', msg.index, msg.id);
     this.index = msg.index;
+  }
+
+  handleMsgPlayerMove(msg) {
+    const {playerIndex, move, winner} = msg;
+    this.board[move] = playerIndex;
+
+    if (winner !== MOVE_EMPTY) {
+      console.log(`I ${winner === this.index ? 'won' : 'lost'} the game.`);
+      return;
+    }
+
+    if (playerIndex !== this.index) {
+      this.sendMove();
+    }
+  }
+
+  sendMove() {
+    this.send({type: 'Move', move: this.getNextMove()})
+  }
+
+  getNextMove() {
+    var i = (Math.random() * FULL_SIZE) | 0;
+    for (;; i = (i + 1) % FULL_SIZE)  {
+      if (this.board[i] === MOVE_EMPTY) {
+        return i;
+      }
+    }
   }
 };
